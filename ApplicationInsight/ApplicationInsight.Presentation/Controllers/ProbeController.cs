a6505bbeba31a09c435b879fb23bfb73f7308f6d
Massimo Bonanni <massimo.bonanni@tiscali.it>
2020-05-21 16:54:51 +0200

    Add probe controller to frontend

---
+++ ApplicationInsight/ApplicationInsight.Presentation/Controllers/ProbeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationInsight.Presentation.Controllers
+{
+    public class ProbeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
