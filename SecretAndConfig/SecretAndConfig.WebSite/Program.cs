7055ff939157e4a219750b4e59413db8e769b938
Massimo Bonanni <massimo.bonanni@tiscali.it>
2021-03-22 09:21:46 +0100

    fix

--- SecretAndConfig/SecretAndConfig.WebSite/Program.cs
+++ SecretAndConfig/SecretAndConfig.WebSite/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -20,40 +21,35 @@ namespace SecretAndConfig.WebSite
             CreateHostBuilder(args).Build().Run();
         }
 
-        //public static IHostBuilder CreateHostBuilder(string[] args) =>
-        //    Host.CreateDefaultBuilder(args)
-        //        .ConfigureWebHostDefaults(webBuilder =>
-        //        {
-        //            webBuilder.UseStartup<Startup>();
-        //        });
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
-                    CurrentEnvironment = hostingContext.HostingEnvironment;
+                CurrentEnvironment = hostingContext.HostingEnvironment;
 
-                    config.AddJsonFile(
-                        "appsettings.json", optional: false, reloadOnChange: true);
-                    config.AddJsonFile(
-                        "appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
-                    config.AddJsonFile(
-                        "appsettings.local.json", optional: true, reloadOnChange: true);
-                    config.AddJsonFile(
-                        "appsettings.local.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                config.AddJsonFile(
+                    "appsettings.json", optional: false, reloadOnChange: true);
+                config.AddJsonFile(
+                    "appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                config.AddJsonFile(
+                    "appsettings.local.json", optional: true, reloadOnChange: true);
+                config.AddJsonFile(
+                    "appsettings.local.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
 
-                    #region [ AppConfiguration environment]
-                    if (hostingContext.HostingEnvironment.IsAppConfigurationEnvironment())
+                #region [ AppConfiguration environment]
+                if (hostingContext.HostingEnvironment.IsAppConfigurationEnvironment())
+                {
+                    var settings = config.Build();
+                    config.AddAzureAppConfiguration(options =>
                     {
-                        var settings = config.Build();
-                        config.AddAzureAppConfiguration(options =>
-                        {
-                            options.Connect(settings["ConnectionStrings:AppConfig"])
-                                   .UseFeatureFlags();
-                        });
-                    }
-                    #endregion [ AppConfiguration environment]
-                })
+                        options.Connect(settings["ConnectionStrings:AppConfig"])
+                                .Select(KeyFilter.Any, LabelFilter.Null)
+                                //.Select(KeyFilter.Any,"Production")
+                                .UseFeatureFlags();
+                });
+    }
+    #endregion [ AppConfiguration environment]
+})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- SecretAndConfig/SecretAndConfig.WebSite/Startup.cs
+++ SecretAndConfig/SecretAndConfig.WebSite/Startup.cs
@@ -44,7 +44,11 @@ namespace SecretAndConfig.WebSite
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
+            else if (env.IsAppConfigurationEnvironment())
+            {
+                app.UseAzureAppConfiguration();
+            }
+            else 
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
